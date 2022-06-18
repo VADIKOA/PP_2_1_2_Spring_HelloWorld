cb7277a1a67bd5823e2bb1d3c9c2087fa50b707b
V@DIK <ageevvo@mail.ru>
2022-06-18 13:34:19 +0400

    Task 2.1.2

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,6 +7,15 @@ public class App {
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
+
+        HelloWorld baenHelloWorld = (HelloWorld) applicationContext.getBean("helloworld");
+
+        Cat cat1 = (Cat) applicationContext.getBean("cat");
+        Cat cat2 = (Cat) applicationContext.getBean("cat");
+
+        System.out.println(bean == baenHelloWorld);
+        System.out.println(cat1 == cat2);
+
         System.out.println(bean.getMessage());
     }
 }
